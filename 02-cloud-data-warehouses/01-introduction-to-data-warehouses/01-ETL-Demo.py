8fc980517829cf1cfd5b270e4dbdd4858fb9a1f7
Heber Trujillo <heber.trj.urt@gmail.com>
2022-02-24 07:30:53 +0100

    Store Dimension table created @ 01-ETL-Demo

--- 02-cloud-data-warehouses/01-introduction-to-data-warehouses/01-ETL-Demo.py
+++ 02-cloud-data-warehouses/01-introduction-to-data-warehouses/01-ETL-Demo.py
@@ -266,6 +266,32 @@ execute_query(
     fetch=False
 )
 
+## Store Dimension
+query = """
+CREATE TABLE dimStore
+(
+  store_key           SERIAL PRIMARY KEY,
+  store_id            smallint NOT NULL,
+  address             varchar(50) NOT NULL,
+  address2            varchar(50),
+  district            varchar(20) NOT NULL,
+  city                varchar(50) NOT NULL,
+  country             varchar(50) NOT NULL,
+  postal_code         varchar(10),
+  manager_first_name  varchar(45) NOT NULL,
+  manager_last_name   varchar(45) NOT NULL,
+  start_date          date NOT NULL,
+  end_date            date NOT NULL
+);
+"""
+
+execute_query(
+    query=query,
+    err_msg="KO",
+    conn=conn,
+    fetch=False
+)
+
 
 query = """
 INSERT INTO dimDate (date_key, date, year, quarter, month, day, week, is_weekend)
