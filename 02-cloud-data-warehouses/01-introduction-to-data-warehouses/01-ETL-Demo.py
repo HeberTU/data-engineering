73640e29b19426b196009f42cbb55701909beefa
Heber Trujillo <heber.trj.urt@gmail.com>
2022-02-24 08:15:00 +0100

    Insert Statement for dimStore table @ 01-ETL-Demo

--- 02-cloud-data-warehouses/01-introduction-to-data-warehouses/01-ETL-Demo.py
+++ 02-cloud-data-warehouses/01-introduction-to-data-warehouses/01-ETL-Demo.py
@@ -350,7 +350,7 @@ execute_query(
 )
 
 
-## Customer Dimension
+## Movie Dimension
 query = """
 INSERT INTO dimMovie (movie_key, film_id, title, description, release_year, language, 
 original_language, rental_duration, length, rating, special_features)
@@ -369,6 +369,38 @@ SELECT
 FROM film f
 JOIN language l              ON (f.language_id=l.language_id)
 LEFT JOIN language orig_lang ON (f.original_language_id = orig_lang.language_id);
+"""
+
+execute_query(
+    query=query,
+    err_msg="KO",
+    conn=conn,
+    fetch=False
+)
+
+
+## Store Dimension
+query = """
+INSERT INTO dimStore (store_key, store_id, address, address2, district, city,
+country, postal_code, manager_first_name, manager_last_name, start_date, end_date)
+SELECT
+    s.store_id as store_key,
+    s.store_id,
+    a.address,
+    a.address2,
+    a.district,
+    ci.city,
+    co.country,
+    a.postal_code,
+    st.first_name as manager_first_name,
+    st.last_name as manager_last_name,
+       now()         AS start_date,
+       now()         AS end_date    
+FROM store s
+LEFT JOIN address a ON (s.address_id = a.address_id)
+LEFT JOIN city ci   ON (a.city_id = ci.city_id)
+LEFT JOIN country co   ON (ci.country_id = co.country_id)
+LEFT JOIN staff st   ON (s.manager_staff_id = st.staff_id);
 """
 
 execute_query(
