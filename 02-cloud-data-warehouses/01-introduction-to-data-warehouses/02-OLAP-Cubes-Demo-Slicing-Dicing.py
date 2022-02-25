89a0774e34ba8cc59a44c4da47c01dcb87902aae
Heber Trujillo <heber.trj.urt@gmail.com>
2022-02-25 08:46:03 +0100

    Slicing and Dicing queries added.

--- 02-cloud-data-warehouses/01-introduction-to-data-warehouses/02-OLAP-Cubes-Demo-Slicing-Dicing.py
+++ 02-cloud-data-warehouses/01-introduction-to-data-warehouses/02-OLAP-Cubes-Demo-Slicing-Dicing.py
@@ -14,3 +14,69 @@ from de_utils.postgres_utils import \
 conn = connect_to_db(
     database='pagila'
 )
+
+
+query = """
+SELECT d.day, m.rating, c.city, sum(s.sales_amount) as revenue  
+from factsales s 
+LEFT join dimdate d on (s.date_key=d.date_key)
+LEFT join dimmovie m on (s.movie_key=m.movie_key)
+LEFT join dimcustomer c on (s.customer_key=c.customer_key)
+GROUP BY day, rating, city
+ORDER BY revenue desc
+limit 5;
+"""
+result = execute_query(
+    query=query,
+    err_msg="KO",
+    conn=conn,
+    fetch=True
+)
+
+print(pd.DataFrame(result))
+
+
+query = """
+SELECT d.day, m.rating, c.city, sum(s.sales_amount) as revenue  
+from factsales s 
+LEFT join dimdate d on (s.date_key=d.date_key)
+LEFT join dimmovie m on (s.movie_key=m.movie_key)
+LEFT join dimcustomer c on (s.customer_key=c.customer_key)
+WHERE rating = 'PG-13'
+GROUP BY day, rating, city
+ORDER BY revenue desc
+limit 5;
+"""
+result = execute_query(
+    query=query,
+    err_msg="KO",
+    conn=conn,
+    fetch=True
+)
+
+print(pd.DataFrame(result))
+
+
+
+query = """
+SELECT d.day, m.rating, c.city, sum(s.sales_amount) as revenue  
+from factsales s 
+LEFT join dimdate d on (s.date_key=d.date_key)
+LEFT join dimmovie m on (s.movie_key=m.movie_key)
+LEFT join dimcustomer c on (s.customer_key=c.customer_key)
+WHERE 
+    rating IN ('PG-13', 'PG') 
+    AND city IN ('Lancaster', 'Bellevue')
+    AND day IN (1, 15, 30)
+GROUP BY day, rating, city
+ORDER BY revenue desc
+limit 5;
+"""
+result = execute_query(
+    query=query,
+    err_msg="KO",
+    conn=conn,
+    fetch=True
+)
+
+print(pd.DataFrame(result))
